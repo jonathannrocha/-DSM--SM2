dd1328d325a52bc2cbd4db7aa33025acf0e42186
Jonathan Rocha <Jonathansantanarocha27@gmail.com>
2024-06-08 12:34:09 -0300

    realizando tarefa 3,4 e 5 - projeto final ESD

--- 2SM/Estrutura de Dados/07. atividade-final/main.cpp
+++ 2SM/Estrutura de Dados/07. atividade-final/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-using namespace std;
 #include <stdio.h>
+#include <cstdio>
+#include <string>
+#include <vector>
+using namespace std;
 
 struct participante {
     int ID;
@@ -28,6 +31,9 @@ void inserirParticipante(lista *, participante);
 void listarTudo(lista *);
 void editarAluno(lista *);
 int alterarProprieade(string propriedade);
+vector< string> split(const string& , char );
+void lerGravarArquivo(lista *);
+void gravarParticipantes(lista * );
 
 int main() {
 
@@ -44,6 +50,8 @@ int main() {
         cout << "| 1 - Inserir participante                |" << endl;
         cout << "| 2 - lista tudo                          |" << endl;
         cout << "| 3 - Editar participante                 |" << endl;
+        cout << "| 4 - ler Arquivo                         |" << endl;
+        cout << "| 5 - Gravar Arquivo                      |" << endl;
         cout << "| # - Sair do menu                        |" << endl;
         cout << "|-----------------------------------------|" << endl;
         cin >> opcaoMenu;
@@ -57,6 +65,12 @@ int main() {
             break;
             case 3: 
                 editarAluno(listagem);
+            break;
+            case 4: 
+                lerGravarArquivo(listagem);
+            break;
+            case 5: 
+                gravarParticipantes(listagem);
             break;
             default:
 
@@ -130,13 +144,16 @@ void listarTudo(lista *listagem) {
     }
      node * itemTemp = listagem->primeiroItem;
     while( itemTemp != NULL) {
-        cout << endl << "[ID] " << itemTemp->item.ID;
-        cout << endl << "[NOME] " << itemTemp->item.primeiroNome;
-        cout << endl << "[CURSO] " << itemTemp->item.curso;
-        cout << endl << "[SEMESTRE] " << itemTemp->item.semestre;
-        cout << endl << "[ANO INGRESSO] " << itemTemp->item.anoIngresso;
+      
+        cout << endl << "--------------------------------------------- ";
+        cout << endl << "ID aluno: " << itemTemp->item.ID;
+        cout << endl << "--------------------------------------------- ";
+        cout << endl << "Nome: " << itemTemp->item.primeiroNome;
+        cout << endl << "Curso: " << itemTemp->item.curso;
+        cout << endl << "Semestre: " << itemTemp->item.semestre;
+        cout << endl << "Ano de Ingreso: " << itemTemp->item.anoIngresso;
+          cout << endl;
         itemTemp = itemTemp->proximo;
-        cout << endl;
     }
 }
 
@@ -164,17 +181,17 @@ void editarAluno(lista * listagem) {
     }
 
     if( alterarProprieade("Nome") == 1 ) {
-        cout << endl << "Qual e o novo valor?" ;
+        cout << endl << "Qual e o novo valor?" << endl ;
         cin >> participantePesquisado->item.primeiroNome;
     }
 
     if( alterarProprieade("Curso") == 1 ) {
-        cout << endl << "Qual e o novo valor?" ;
+        cout << endl << "Qual e o novo valor?" << endl ;
         cin >> participantePesquisado->item.curso;
     }
 
     if( alterarProprieade("Semestre") == 1 ) {
-        cout << endl << "Qual e o novo valor?";
+        cout << endl << "Qual e o novo valor?" << endl;
         cin >> participantePesquisado->item.semestre;
     }
     if( alterarProprieade("Ano de Ingreso") == 1 ) {
@@ -192,3 +209,102 @@ int alterarProprieade(string propriedade) {
     return escolha;
 };
 
+void lerGravarArquivo(lista * listagem) {
+  
+    FILE *arq = fopen("../text.txt", "rt");         
+    if (arq == NULL) {
+       cout << "Problemas na abertura do arquivo!" << endl;
+    }
+    int vezes = 0;
+    char linha[100];
+    char *rowResult;
+    participante novoParticipante;
+
+    while ((rowResult = fgets(linha, 100, arq)) != NULL)  {
+        // rowResult = fgets(linha, 100, arq);
+        string input = rowResult;
+
+        char delimiter = ',';
+        if(input != " ") {
+            vector<string> result = split(input, delimiter);
+            vezes++;
+            for (const auto& s : result) {
+                string str = s;
+                string valor;
+                string propriedade;
+                size_t end = str.find(":");
+                valor = str.substr(end+1);
+                propriedade = str.substr(0, end);
+                cout << valor;
+                novoParticipante.ID = 0;
+                if(propriedade == "nome" ) novoParticipante.primeiroNome = valor;
+                if(propriedade == "semestre" ) novoParticipante.semestre = stol(valor);
+                if(propriedade == "anoIngresso" ) novoParticipante.anoIngresso = stol(valor);
+                if(propriedade == "curso" )  {
+                    novoParticipante.curso = valor.substr(0,2);
+                    // cout << valor;
+                }
+            }
+
+            inserirParticipante(listagem, novoParticipante);
+        }
+    }
+
+    if( vezes == 0 )  {
+         cout << endl << "Nao foi encontrado nenhum valor!!!";
+    } else {
+        cout << endl << "Foi inserido  " << vezes << " Participantes!!";
+    }
+    fclose(arq);
+};
+
+vector<string> split(const string& str, char delimiter) {
+    vector<string> tokens;
+    string token;
+    size_t start = 0;
+    size_t end = str.find(delimiter);
+    
+    while (end != string::npos) {
+        token = str.substr(start, end - start);
+        tokens.push_back(token);
+        start = end + 1;
+        end = str.find(delimiter, start);
+    }
+    
+    // Adiciona o último token
+    token = str.substr(start, end - start);
+    tokens.push_back(token);
+    
+    return tokens;
+}
+
+
+void gravarParticipantes(lista * listagem) {
+
+    if( listagem->qtd== 0) {
+        cout << endl << "Nenhum aluno registrado!!!";
+        return;
+    }
+
+    FILE *arq = fopen("../text.txt", "w");         
+    if (arq == NULL) {
+       cout << "Problemas na abertura do arquivo!" << endl;
+    }
+
+    
+    node * itemTemp = listagem->primeiroItem;
+    while( itemTemp != NULL) {
+
+        string valor = "nome:" + itemTemp->item.primeiroNome;
+        valor = valor + ",semestre:" + to_string(itemTemp->item.semestre);
+        valor = valor + ",anoIngresso:"+to_string(itemTemp->item.anoIngresso);
+        valor = valor + ",curso:"+itemTemp->item.curso;
+        valor = valor + "\n";
+
+        string temp = valor;
+        fputs(valor.c_str(), arq);
+        itemTemp = itemTemp->proximo;
+    }
+
+    fclose(arq);
+}
